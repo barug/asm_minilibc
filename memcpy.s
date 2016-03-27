efed9c138e035d09f3ec53960dc00f9597afa221
Barthélemy Gouby <barthelemy.gouby@gmail.com>
2016-03-27 17:27:28 +0200

    should be good

--- memcpy.s
+++ memcpy.s
@@ -4,13 +4,15 @@
 
 memcpy:
 	mov rcx, 0
-
+	mov r8, 0
+	
 loop:
-	cmp rcx, rdx
+	cmp r8, rdx
 	je return
-	mov al, [rsi+rdx]
+	mov al, [rsi+r8]
 	mov [rdi+rcx], al
 	inc rcx
+	inc r8
 	jmp loop
 
 return:
